765a33cac4b71d3550a88f40cb97d2debed4dcd6
Elchanan5760 <emstone@gmail.com>
2025-06-10 00:22:30 +0300

    DAL and row model

---
+++ DAL/DAL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Malshinon.models;
+using MySql.Data.MySqlClient;
+
+namespace Malshinon.DAL
+{
+    public class DAL
+    {
+        private PeopleRow _row;
+        private string connectionString = "Server=localhost;Database=malshinon;User=root;Password='';Port=3306;";
+        public object Connection(string cmdSql)
+        {
+            MySqlConnection conn = new MySqlConnection(connectionString);
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(cmdSql);
+                var reader = cmd.ExecuteReader();
+                conn.Close();
+                return reader;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Error connecting to MySql dadabase: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
